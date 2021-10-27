c3f8c34cefd818e32980b03c3e1cfc12765afbb5
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-10-26 18:40:53 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function preload(){
+
+}
+
+function setup(){
+  canvas = createCanvas(500, 400);
+  canvas.center()
+  video = createCapture(VIDEO);
+  //video.position()
+  //this to to set position of video
+}
+
+function draw(){
+  background('white')
+}
