fe6cf5c0c98132e371e3ac9aca4ace123ad4f964
AneeshCodes <74843895+AneeshCodes@users.noreply.github.com>
2021-11-01 18:38:39 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+ToType = ''
+x = 100
+y = 100
+
 function preload(){
 
 }
@@ -8,8 +12,24 @@ function setup(){
   video = createCapture(VIDEO);
   //video.position()
   //this to to set position of video
+  poseNet = ml5.poseNet(video, modelLoaded)
+  poseNet.on('pose', gotResults)
+}
+
+function modelLoaded(){
+  console.log('Model is Loaded')
 }
 
 function draw(){
-  background('white')
+  background('white');
+  ToType = document.getElementById('toType').value
+  console.log(ToType)
+  text(ToType, x, y);
+}
+
+function gotResults(results){
+  if(results.length > 0){
+    console.log(results)
+
+  }
 }
